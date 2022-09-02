4ba17581a38e0182e39c1f8817b7775c16c87c40
ZeframLou <zeframlou@gmail.com>
2022-09-02 16:26:22 -0700

    build: add deploy scripts

---
+++ deploy/mainnet.sh
@@ -0,0 +1,13 @@
+# load environment variables from .env
+source .env
+
+# set env variables
+export ADDRESSES_FILE=./deployments/mainnet.json
+export RPC_URL=$RPC_URL_MAINNET
+
+# load common utilities
+. $(dirname $0)/common.sh
+
+# deploy contracts
+bagholder_address=$(deploy Bagholder $PROTOCOL_FEE)
+echo "Bagholder=$bagholder_address"

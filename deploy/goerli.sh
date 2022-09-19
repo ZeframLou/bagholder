e14b4c056f66339b1db7a2868ca61bbc20ced02f
ZeframLou <zeframlou@gmail.com>
2022-09-18 17:56:50 -0700

    build: deploy to mainnet, update deploy scripts

--- deploy/goerli.sh
+++ deploy/goerli.sh
@@ -11,3 +11,6 @@ export RPC_URL=$RPC_URL_GOERLI
 # deploy contracts
 bagholder_address=$(deploy Bagholder $PROTOCOL_FEE)
 echo "Bagholder=$bagholder_address"
+
+send $bagholder_address "transferOwnership(address,bool,bool)" $OWNER true false
+echo "BagholderOwner=$OWNER"
